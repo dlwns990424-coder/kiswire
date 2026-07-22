5490f86f023e1f0ff5fff16b94b137d1de761c52
jun <dlwns990424@naver.com>
2026-07-22 10:59:53 +0900

    add: tablet 반응형 추가 및 ham_menu 작업중

--- script/main.js
+++ script/main.js
@@ -96,3 +96,33 @@ const familyHandler = (e) => {
 };
 
 family.addEventListener("click", familyHandler);
+
+// swiper
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.4,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
